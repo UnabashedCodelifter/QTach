6db77b4bda9217ae4dfd7c77d637c94b678cb4be
Norbert Hartlep <norbert.hartlep13@gmail.com>
2023-01-07 23:35:48 +0100

    more customer reviews

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//kiedy karty som niewidoczne
+
+const invisibleCard =[true,true,true,true];
+
 const cards = document.querySelectorAll(".card");
 cards[0].addEventListener("click", function(){showDesc(0)});
 cards[1].addEventListener("click", function(){showDesc(1)});
@@ -6,5 +10,13 @@ cards[3].addEventListener("click", function(){showDesc(3)});
 
 function showDesc(card)
 {
-    document.querySelectorAll(".card span")[card].style.color = "#414141";
+    let span = document.querySelectorAll(".card span")[card];
+    if (span.style.color != "rgb(65, 65, 65)")
+    {
+        span.style.color = "#414141";
+    }
+    else
+    {
+        span.style.color = "transparent";
+    }
 }
